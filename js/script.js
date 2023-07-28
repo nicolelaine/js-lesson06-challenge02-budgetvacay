31b8023cb34e1313b0db3e1126e589e476b98d01
nicole_s <15011784+nicolelaine@users.noreply.github.com>
2023-07-28 12:37:21 +0200

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,18 @@
+var tripInfoButton = document.querySelector(".trip-info");
+var dailyBudget = document.querySelector(".daily-budget");
+
+var tripInfo = function () {
+  var totalBudget = Number(prompt(`What is your total budget?`));
+  var accomodation = Number(prompt(`What are your accomodation costs?`));
+  var numDays = Number(prompt(`How many deays will your trip last?`));
+  calculateDailyBudget(totalBudget, accomodation, numDays);
+};
+
+var calculateDailyBudget = function (totalBudget, accomodation, numDays) {
+  var daily = ((totalBudget - accomodation) / numDays).toFixed(2);
+  dailyBudget.innerText = `You can spend $${daily} a day on food and fun!`;
+};
+
+tripInfoButton.addEventListener("click", function () {
+  tripInfo();
+});
